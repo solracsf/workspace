3942d67be98ffc92005051babb75a381b0d88468
zak39 <baptiste.fotia@arawa.fr>
2022-04-07 14:35:24 +0200

    feat(Controller): Add @NoCSRFRequired anotation in saveSettings method

--- lib/Controller/WorkspaceSettingsController.php
+++ lib/Controller/WorkspaceSettingsController.php
@@ -7,6 +7,7 @@ class WorkspaceSettingsController extends Controller {
      * Save Settings
      * @PasswordConfirmationRequired
      * @AuthorizedAdminSetting(settings=OCA\Workspace\Settings\WorkspaceAdmin)
+     * @NoCSRFRequired
      */
     public function saveSettings($mySetting) {
         return $mySetting;
